eaca2182c20014fa9cf0f3cb634781ff18683e07
wanghao8080 <117663444@qq.com>
2017-09-10 14:19:07 +0800

    获取数据

--- app.js
+++ app.js
@@ -1,1 +1,13 @@
-App({})
+App({
+  globalData: {
+
+  },
+  onLaunch() {
+    wx.request({
+      url: 'https://resources.ninghao.net/wxapp-case/db.json',
+      success: (response) => {
+        Object.assign(this.globalData, response.data)
+      }
+    })
+  }
+})
