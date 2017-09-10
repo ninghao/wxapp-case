00d79b51c314399b98d00342d8723ef426e41b9a
wanghao8080 <117663444@qq.com>
2017-09-10 20:59:43 +0800

    改进：全局的网络请求

--- app.js
+++ app.js
@@ -7,6 +7,11 @@ App({
       url: 'https://resources.ninghao.net/wxapp-case/db.json',
       success: (response) => {
         Object.assign(this.globalData, response.data)
+
+        const currentPages = getCurrentPages()
+        if (currentPages.length !== 0) {
+          currentPages[currentPages.length - 1].onLoad()
+        }
       }
     })
   }
