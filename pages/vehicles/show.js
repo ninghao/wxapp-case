d9b9eeaa6f4a12a861faa85206f216007c950904
wanghao8080 <117663444@qq.com>
2017-09-11 18:58:33 +0800

    产品详情页：数据

--- pages/vehicles/show.js
+++ pages/vehicles/show.js
@@ -1,3 +1,21 @@
 const app = getApp()
 
-Page({})
+Page({
+  data: {
+    entity: null
+  },
+  onLoad() {
+    const id = 3
+    const entity = app.globalData.vehicles.filter((item) => {
+      return item.id == id
+    })
+
+    this.setData({
+      entity: entity[0]
+    })
+
+    wx.setNavigationBarTitle({
+      title: this.data.entity.header
+    })
+  }
+})
