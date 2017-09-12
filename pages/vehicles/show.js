98beb6393944cb7bc4deeecef664de5accffdefa
wanghao8080 <117663444@qq.com>
2017-09-12 16:59:35 +0800

    产品详情页：图片预览

--- pages/vehicles/show.js
+++ pages/vehicles/show.js
@@ -4,6 +4,22 @@ Page({
   data: {
     entity: null
   },
+  preview(event) {
+    const slidesName = event.target.dataset.slides
+    const index = event.target.dataset.index
+
+    const slides = this.data.entity.meta[slidesName]
+    const images = []
+
+    slides.map((item) => {
+      images.push(item.image)
+    })
+
+    wx.previewImage({
+      urls: images,
+      current: images[index]
+    })
+  },
   onLoad() {
     const id = 3
     const entity = app.globalData.vehicles.filter((item) => {
