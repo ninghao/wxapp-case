d9f313755493620ae0b803000361076f5121299a
wanghao8080 <117663444@qq.com>
2017-09-12 18:03:13 +0800

    同时只能播放一个视频

--- pages/stories/index.js
+++ pages/stories/index.js
@@ -2,12 +2,23 @@ const app = getApp()
 
 Page({
   data: {
-    entities: null
+    entities: null,
+    currentVid: null,
+    currentVideo: null
   },
   play(event) {
+    if (this.data.currentVid !== null) {
+      this.data.currentVideo.pause()
+    }
+
     const currentVideo = wx.createVideoContext(
       `${ event.target.dataset.vid }`)
     currentVideo.play()
+
+    this.setData({
+      currentVideo,
+      currentVid: event.target.dataset.vid
+    })
   },
   onLoad() {
     this.setData({
