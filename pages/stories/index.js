2ab16d787a329f4a71bbd9b39bcdc72a5bd93355
wanghao8080 <117663444@qq.com>
2017-09-12 17:57:28 +0800

    故事列表：视频

--- pages/stories/index.js
+++ pages/stories/index.js
@@ -4,6 +4,11 @@ Page({
   data: {
     entities: null
   },
+  play(event) {
+    const currentVideo = wx.createVideoContext(
+      `${ event.target.dataset.vid }`)
+    currentVideo.play()
+  },
   onLoad() {
     this.setData({
       entities: app.globalData.stories
